c074b43cf10ebba0d693549d457ba573db00a5ef
Frecautanu Ovidiu <70950196+Ovidiu2004@users.noreply.github.com>
2020-12-02 19:13:20 +0300

    Add files via upload

---
+++ string 1 (caiet).py
@@ -0,0 +1,6 @@
+m=str(input("cuvintul:"))
+n=str(input("litera:"))
+
+for i in m:
+    c=m.replace(i,n)
+    print(c)
---
+++ string 2(caiet).py
@@ -0,0 +1,7 @@
+import string
+m=str(input("dati numele unei persoane:"))
+n=m.title()
+if m==n:
+    print("Ar putea fi un nume de persoana")
+else:
+    print("Nu ar putea fi un nume de persoana")
